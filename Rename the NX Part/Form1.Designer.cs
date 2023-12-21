34f0b3060880ffc1dbdb2d002022df7461557e85
MohanDulam <mohandulam24@gmail.com>
2023-12-21 20:53:31 +0530

    Add project files.

---
+++ Rename the NX Part/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿
+namespace Rename_the_NX_Part
+{
+    partial class NX_File_Rename
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(NX_File_Rename));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtRenameFilePath = new System.Windows.Forms.TextBox();
+            this.btnBrowseFile = new System.Windows.Forms.Button();
+            this.txtNewFileName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.btnRename = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnInfo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(18, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(181, 27);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "File to Rename";
+            // 
+            // txtRenameFilePath
+            // 
+            this.txtRenameFilePath.Location = new System.Drawing.Point(214, 31);
+            this.txtRenameFilePath.Name = "txtRenameFilePath";
+            this.txtRenameFilePath.Size = new System.Drawing.Size(380, 26);
+            this.txtRenameFilePath.TabIndex = 1;
+            // 
+            // btnBrowseFile
+            // 
+            this.btnBrowseFile.Font = new System.Drawing.Font("Century", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBrowseFile.Location = new System.Drawing.Point(604, 24);
+            this.btnBrowseFile.Name = "btnBrowseFile";
+            this.btnBrowseFile.Size = new System.Drawing.Size(114, 39);
+            this.btnBrowseFile.TabIndex = 2;
+            this.btnBrowseFile.Text = "Browse";
+            this.btnBrowseFile.UseVisualStyleBackColor = true;
+            this.btnBrowseFile.Click += new System.EventHandler(this.btnBrowseFile_Click);
+            // 
+            // txtNewFileName
+            // 
+            this.txtNewFileName.Location = new System.Drawing.Point(214, 94);
+            this.txtNewFileName.Name = "txtNewFileName";
+            this.txtNewFileName.Size = new System.Drawing.Size(380, 26);
+            this.txtNewFileName.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(18, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(184, 27);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "New File Name";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Font = new System.Drawing.Font("Century", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatus.ForeColor = System.Drawing.Color.White;
+            this.lblStatus.Location = new System.Drawing.Point(20, 150);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(106, 27);
+            this.lblStatus.TabIndex = 5;
+            this.lblStatus.Text = "Status...";
+            // 
+            // btnRename
+            // 
+            this.btnRename.Font = new System.Drawing.Font("Century", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRename.Location = new System.Drawing.Point(225, 193);
+            this.btnRename.Name = "btnRename";
+            this.btnRename.Size = new System.Drawing.Size(284, 71);
+            this.btnRename.TabIndex = 6;
+            this.btnRename.Text = "Rename";
+            this.btnRename.UseVisualStyleBackColor = true;
+            this.btnRename.Click += new System.EventHandler(this.btnRename_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Century", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(565, 207);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(125, 45);
+            this.btnClear.TabIndex = 7;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnInfo
+            // 
+            this.btnInfo.Font = new System.Drawing.Font("Century", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInfo.Location = new System.Drawing.Point(36, 207);
+            this.btnInfo.Name = "btnInfo";
+            this.btnInfo.Size = new System.Drawing.Size(125, 45);
+            this.btnInfo.TabIndex = 8;
+            this.btnInfo.Text = "Info";
+            this.btnInfo.UseVisualStyleBackColor = true;
+            this.btnInfo.Click += new System.EventHandler(this.btnInfo_Click);
+            // 
+            // NX_File_Rename
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Firebrick;
+            this.ClientSize = new System.Drawing.Size(728, 289);
+            this.Controls.Add(this.btnInfo);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnRename);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.txtNewFileName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnBrowseFile);
+            this.Controls.Add(this.txtRenameFilePath);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "NX_File_Rename";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "NX File Rename";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtRenameFilePath;
+        private System.Windows.Forms.Button btnBrowseFile;
+        private System.Windows.Forms.TextBox txtNewFileName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Button btnRename;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnInfo;
+    }
+}
+
---
+++ Rename the NX Part/Form1.cs
@@ -0,0 +1,277 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Rename_the_NX_Part
+{
+    public partial class NX_File_Rename : Form
+    {
+        public NX_File_Rename()
+        {
+            InitializeComponent();
+        }
+
+        private string pathToNxFileToRename;
+        private string newPartName;
+        private bool filePath = false;
+
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new NX_File_Rename());
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnBrowseFile_Click(object sender, EventArgs e)
+        {
+            txtNewFileName.Clear();
+            lblStatus.Text = "Status...";
+            lblStatus.ForeColor = Color.White;
+
+            using (OpenFileDialog fileDialogWindow = new OpenFileDialog())
+            {
+                DialogResult result = fileDialogWindow.ShowDialog();
+
+                if (result == DialogResult.OK)
+                {
+                    string fileExtensionCheck = fileDialogWindow.FileName.Trim();
+                    string fileExtension = Path.GetExtension(fileExtensionCheck);
+
+                    if (fileExtension.Contains(".prt"))
+                    {
+                        txtRenameFilePath.Text = fileExtensionCheck;
+                        pathToNxFileToRename = txtRenameFilePath.Text;
+                        filePath = true;
+                    }
+                    else
+                    {                        
+                        MessageBox.Show("Please Select Proper Part File", "Error", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                        lblStatus.Text = "Please Select Proper Part File :(";
+                        lblStatus.ForeColor = Color.White;
+                    }
+                }
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtRenameFilePath.Clear();
+            txtNewFileName.Clear();
+            lblStatus.Text = "Status...";
+            lblStatus.ForeColor = Color.White;
+        }
+
+        private void btnInfo_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("This Code was Created By \"Mohan Dulam\" \n" +
+                "     ******Instructions to Use this Application******  \n"+
+                "1. Clik on Browse bottom to select the part file to Rename.\n"+
+                "2. Provide the New Name for Part File in New File Name.\n" +
+                "3. Click on \"Rename\" Bottom.\n" +
+                "4. Message of Successfull Rename of Part File.", "Information", 
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btnRename_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(txtNewFileName.Text) && filePath)
+            {
+                newPartName = txtNewFileName.Text;
+
+                FileReName(pathToNxFileToRename, newPartName);
+
+                lblStatus.Text = "Renaming of File is successfull :) ";
+                lblStatus.ForeColor = Color.Green;
+
+                MessageBox.Show("Renaming of File is successfull :) ", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            }
+            else
+            {
+                if (filePath)
+                {
+                    MessageBox.Show("Please Provide New Name for Part File", "Error", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                    lblStatus.Text = "Please Provide New Name for Part File :( ";
+                    lblStatus.ForeColor = Color.GhostWhite;
+                }
+                else
+                {
+                    MessageBox.Show("Please Provide Rename File and Path, New Name for Part File", "Error", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                    lblStatus.Text = "Rename File and Path, New Name for Part File Missing :( ";
+                    lblStatus.ForeColor = Color.GhostWhite;
+                }
+            }
+        }
+
+        /// <summary>
+        /// To Rename the Part File of NX
+        /// </summary>
+        /// <param name="pathToNxFileToRename">Rename File with Path</param>
+        /// <param name="name">New Name for Part</param>
+        private static void FileReName(string pathToNxFileToRename, string name)
+        {
+            // Adding Part Extension to the Given New Part Name
+            name = name + ".prt";
+
+            // Split selected files directory and name file with extension
+            // into separate string variables
+            string directory = Path.GetDirectoryName(pathToNxFileToRename);
+            string nxFileToRename = Path.GetFileName(pathToNxFileToRename);
+
+            // Convert the paths to Windows style
+            directory = directory.Replace("/", "\\");
+
+            // Collect all other NX files in the same directory
+            List<string> nxFiles = new List<string>();
+
+            // Specify the file extension to filter only Part file in the directory
+            string fileExtension = "*.prt";
+
+            // Get all the files with the specified extension in the source directory and its subdirectories
+            string[] files = Directory.GetFiles(directory, fileExtension, SearchOption.AllDirectories);
+
+            // Get all the files with the specified extension in the source directory and its subdirectories
+            // string[] files = Directory.GetFiles(directory, "*.*", SearchOption.AllDirectories);
+
+            // Iterate over the files
+            foreach (string file in files)
+            {
+                // Check if the file extension is .prt (adjust as needed)
+                if (Path.GetExtension(file).Equals(".prt", StringComparison.OrdinalIgnoreCase))
+                {
+                    // Add the file path to the list
+                    nxFiles.Add(file);
+                }
+            }
+
+            // Remove the file need to be renamed from the list
+            nxFiles.Remove(nxFileToRename);
+
+            // get the file and path of the ug_edit_part_names.exe
+            string argFirstSection = Environment.GetEnvironmentVariable("UGII_BASE_DIR") + @"\NXBIN\ug_edit_part_names.exe ";
+
+            // Iterate over the nxfiles
+            foreach (string nxFile in nxFiles)
+            {
+                // Construct the second argument section
+                string argSecondSection = $"\"{Path.Combine(directory, nxFile)}\"";
+
+                // Build the command with options of the ug_edit_part_names.exe          
+                string getArgument = argSecondSection + " -list";
+
+                try
+                {
+                    string output, error; // output and error messages from ug_edit_part_names.exe
+
+                    // Calling the NewProcessor for runing the ug_edit_part_names.exe
+                    NewProcessor(argFirstSection, getArgument, out output, out error);
+
+                    // Check for the output from the application is not null
+                    if (!string.IsNullOrEmpty(output))
+                    {
+                        // List to store all the induvidual part in directory
+                        List<string> childs = new List<string>();
+                        string partName; // variable to collect only part name with extension
+                        // Split the output of the application based on multiple lines 
+                        string[] lines = output.Split('\n');
+                        // Iterate over the each line of the output of application
+                        foreach (string line in lines)
+                        {
+                            // Check for line is not null
+                            if (!string.IsNullOrWhiteSpace(line))
+                            {
+                                // output of application contains the part name with extension and the path of file
+                                // split the part name with extension and path of file
+                                partName = line.Substring(0, line.LastIndexOf(".prt")) + ".prt";
+                                // Add part name to childs list
+                                childs.Add(partName);
+                            }
+                        }
+
+                        // check for the length of the childs list  
+                        if (childs.Count > 1)
+                        {
+                            // // Iterate over the child list
+                            foreach (string childPart in childs)
+                            {
+                                // check for childpart name and required rename part are same
+                                if (nxFileToRename == childPart)
+                                {
+                                    // Argument to application for renaming the part
+                                    string renameArgument = argSecondSection + " -o " + argSecondSection + " -change_name " + $"\"{nxFileToRename}\" " + $"\"{name}\"";
+                                    //Console.WriteLine("Remane part Argument is "+argFirstSection + renameArgument);
+
+                                    // Calling the NewProcessor for runing the ug_edit_part_names.exe
+                                    NewProcessor(argFirstSection, renameArgument, out output, out error);
+
+                                    // After renaming the required part break the loop
+                                    break;
+                                }
+                            }
+                        }
+                        childs.Clear();// Clear the list                     
+                    }
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Error in Renaming Part File", "Error", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                    //throw;
+                }
+            }
+
+            // Rename the part file on the directory
+            string fromName = Path.Combine(directory, nxFileToRename);
+            string toName = Path.Combine(directory, name);
+            File.Move(fromName, toName);
+        }
+        /// <summary>
+        /// To Start New Processor 
+        /// </summary>
+        /// <param name="argFirstSectionOfAppllication">Appliction with Path</param>
+        /// <param name="argSecondSection">Commond for the Application</param>
+        /// <param name="outputOfApplication">Output of the Application</param>
+        /// <param name="errorOfApplication">Errror of application</param>
+        private static void NewProcessor(string argFirstSectionOfAppllication, string argSecondSection, out string outputOfApplication, out string errorOfApplication)
+        {
+            outputOfApplication = null;
+            errorOfApplication = null;
+            try
+            {
+                // Arguments for the Processor 
+                ProcessStartInfo startInfo = new ProcessStartInfo();
+                startInfo.FileName = argFirstSectionOfAppllication;
+                startInfo.Arguments = argSecondSection;
+                startInfo.RedirectStandardInput = true;
+                startInfo.RedirectStandardOutput = true;
+                startInfo.RedirectStandardError = true;
+                startInfo.UseShellExecute = false;
+
+                // Intillazation of the New Processor
+                Process process = new Process();
+                process.StartInfo = startInfo;
+                process.Start(); // Start Processor
+
+                // Output and error from the Application
+                outputOfApplication = process.StandardOutput.ReadToEnd();
+                errorOfApplication = process.StandardError.ReadToEnd();
+
+                process.WaitForExit();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error in handling the Processor", "Error", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                //throw;
+            }
+        }
+
+    }
+}
